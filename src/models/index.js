d9c61a521b95918a35700c875baebbe0a5827d38
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-06-07 17:46:25 -0300

    advances

---
+++ src/models/index.js
@@ -0,0 +1,27 @@
+const Album = require("./Album");
+const Artist = require("./Artist");
+const Genre = require("./Genre");
+const Song = require("./Song");
+
+//genreArtist
+Genre.belongsToMany(Artist, { through: "genreArtist" })
+Artist.belongsToMany(Genre, { through: "genreArtist" })
+
+
+//album -> artistId
+Album.belongsTo(Artist) //artistId
+Artist.hasMany(Album)
+
+//song -> albumId
+Song.belongsTo(Album) //albumId
+Album.hasMany(Song)
+
+//songArtist
+Song.belongsToMany(Artist, { through: 'songArtist' })
+Artist.belongsToMany(Song, { through: 'songArtist' })
+
+//songGenres
+Song.belongsToMany(Genre, { through: 'songGenre' })
+Genre.belongsToMany(Song, { through: 'songGenre' })
+
+
